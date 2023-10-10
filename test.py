ac4a0a23e1638edb95743cdc3a483a0a3a17fe15
2563638g <2563638G@student.gla.ac.uk>
2023-10-10 18:34:22 +0200

    Función añadida que coge las palabras de una frase entre los indexes que le das, te las traduce del ingles al español y las substituye

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 import os
+from googletrans import Translator
 
 files = []
 bad_chars = ["&0","&1","&2","&3","&4","&5","&6","&7","&8","&9","&a","&b","&c","&l","&m","&n","&o"]
 
 def tidy_up(value):
     if (value != "&") or (value != "/") or (value != "{") or (value != '"') or (value != "'"):
-        True
+        return True
+
+def tidy_up_v2(value):
+    if value not in ["&","/","{","}","'",'"']:
+        return True
 
 def filter_txt(line):
     if '"' in line:
@@ -21,7 +26,7 @@ def filter_txt(line):
             print(extract(line))
             print("---------------------")
             print("---------------------")
-              
+
 def extract(line):
     elts = []
     next_and = 0
@@ -42,7 +47,18 @@ def extract(line):
         
         line = line[next_and + 2:]
     return elts
-        
+
+def insert(original_phrase, indexes):
+    translator = Translator() #creo que lo suyo sería sacar esto
+    translated_phrase = original_phrase
+    total_length_diff = 0
+    length_diff = 0
+    for i,chunk in enumerate(indexes):
+        chunk_translated = translator.translate(original_phrase[indexes[i][0]:indexes[i][1]], dest="es", src="en").text
+        translated_phrase = translated_phrase[:(indexes[i][0] + total_length_diff)] + chunk_translated + translated_phrase[(indexes[i][1] + length_diff):]
+        length_diff = len(chunk_translated) - indexes[i][1] + indexes[i][0]
+        total_length_diff += length_diff
+    return translated_phrase
 
 def get_string(files):
     for i in files:
@@ -71,7 +87,6 @@ class phr:
     
     def __init__ (self, content, line_num, start_index, file_path):
         self.raw = content
-
         self.divided = []
         self.translated = ""
         self.line_num = line_num    
@@ -79,5 +94,6 @@ class phr:
         self.file_path = file_path
 
 #get_string("./archivos/Quests/quests/collect66rawrabbit.yml")
+print(insert("hello world", ["hello","world"], [[0,4],[6,11]]))
 
 
