004c6a1b8095b0060a6cb4940d8424c9419e79b8
2563638g <2563638G@student.gla.ac.uk>
2023-10-15 23:16:45 +0200

    Movido los archivos a un folder Añadida una barra de progreso bugeadisima Añadida la limpieza de frases de alberto para la busqueda con un par de modificaciones

--- finder.py
+++ finder.py
@@ -50,25 +50,78 @@ class Table:
         for i,resultado in enumerate(self.results):
             resultado.draw(row+i+1, col)
 
+class ProgressbarWrapper:
 
-def find_matches(files, text, match_threshold=0.5):
+    def __init__(self, display, row, col):
+        self.object = Progressbar(display, orient="horizontal", mode="determinate", length=100)
+        self.object.step(0)
+        self.display = display
+        self.length = 0
+        self.value = 0
+        self.row = row
+        self.column = col
+
+    def increase(self):
+        self.value += 1
+        self.object.step((self.value/self.length)*100)
+        self.object.grid(row=self.row, column=self.column, columnspan=3)
+        if self.value == self.length:
+            self.object.grid_forget()
+
+    def set_length(self, length):
+        self.length = length
+
+
+def extract(line):
+    good_chars = ["&0", "&1", "&2", "&3", "&4", "&5", "&6", "&7", "&8", "&9", "&a", "&b", "&c", "&e", "&l", "&m", "&n","&o", '\n']
+    bad_chars = ["&", "%", "/", "<", ">", "[", "]", "{", "}"]
+    indexes = []
+    array_to_trans = []
+    #este for loop crea una lista llamada indexes con la posicion de los chars raros (los de la lista bad_chars)
+    for i in range(len(line)):
+        if line[i] in bad_chars:
+            indexes.append(i)
+
+    #este loop coge todas las frases que hay entre los characteres raros
+    for i in range(len(indexes)):
+        #pero en vd solo importan los que estan despues de & asin que solo coje esos
+        if line[indexes[i]] == "&":
+            a = (line[indexes[i]] + line[indexes[i]+1])
+            if a in good_chars and indexes[-1] != indexes[i]:
+                if (len(line[indexes[i] + 2:indexes[i+1]])) > 1 and (line[indexes[i] + 2:indexes[i+1]] != " "):
+                    array_to_trans.append(line[indexes[i] + 2:indexes[i+1]])
+            if indexes[-1] == indexes[i]:
+                if (len(line[indexes[i] + 2:])) > 1 and (line[indexes[i] + 2:] != " "):
+                    array_to_trans.append(line[indexes[i] + 2:])
+        else:
+            array_to_trans.append("")
+    return array_to_trans
+
+def find_matches(files, text, progress_display, match_threshold=0.2):
     output = []
     split_text = text.split()
+    if files is None:
+        raise "No se encontraron archivos"
     for i in files:
+        print(f"leyendo {i}")
         text = open(i, "r")
         lines = text.readlines()
         text.close()
         for line in lines:
-            split_line = line.split()
+            split_line = extract(line)
             matches = 0
+            similarity = 0
             for i in range(len(split_text)):
-                if i > len(split_line):
+                if i >= len(split_line):
                     pass
                 elif split_text[i] == split_line[i]:
+                    print("match")
                     matches += 1
-            similarity = matches/len(split_text)
+            if matches != 0:
+                similarity = matches/len(split_text)
             if similarity > match_threshold:
                 output.append([line,i,similarity])
+        progress_display.increase()
     return output
 
 
@@ -89,13 +142,14 @@ def reader(dir):
     return output
 
 
-def search_files(search_text, table, display, dir=None):
+def search_files(search_entry, table, result_display, progress_display, dir=None):
     files = reader(dir)
-    matches = find_matches(files, search_text)
+    progress_display.set_length(len(files))
+    matches = find_matches(files, search_entry.get(), progress_display)
+    print(matches)
     for elt in matches:
-        table.inser(elt[0], elt[1], elt[2], display_item_setup(display))
-    #table.insert("a","b","c",display_item_setup(display))
-    table.draw(4,0)
+        table.insert(elt[0], elt[1], elt[2], display_item_setup(result_display))
+    table.draw(5,0)
 
 
 def display_item_setup(label):
@@ -113,13 +167,15 @@ def main():
 
     table = Table(master)
 
+    progressbar = ProgressbarWrapper(master,4,0)
+
     search = Entry(master)
     search.grid(row=0, column=1, pady=5, padx=5)
 
     selected = Label(master, text="")
     selected.grid(row=2, column=0)
 
-    button = Button(master, text="Buscar", command=lambda: search_files(table,selected))
+    button = Button(master, text="Buscar", command=lambda: search_files(search,table,selected, progressbar))
     button.grid(row=0, column=2, pady=5, padx=5)
 
     selected_label = Label(master, text="Texto seleccionado")
@@ -137,7 +193,7 @@ def main():
     replacement = Entry(master)
     replacement.grid(row=2, column=2)
 
-    table.draw(4, 0)
+    table.draw(5, 0)
 
     # infinite loop which can be terminated by keyboard
     # or mouse interrupt
@@ -145,7 +201,8 @@ def main():
 
 # y luego tienes un text box con un botton de sustituir
 # despues de eso viene implementación de las funciones
-#  - primero buscar por los archivos por un match
+#  - primero buscar por los archivos por un match, el match no se detecta no se por que
+#  - la barra de progeso esta bugeadisima
 #  - luego sustituirlo
 
 main()
