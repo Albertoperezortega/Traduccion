85b196b38099de2562438050d62a41377ec0cf88
2563638g <2563638G@student.gla.ac.uk>
2023-10-16 22:25:45 +0200

    He metido lo de codigo_separar_txt en el main (test) y lo he mejorado añadiendo una función que filtra los resultados para quitar cosas raras o resultados vacios, funciona bastante bien pero algunos indices estan mal.

--- Translate.py
+++ Translate.py
@@ -2,9 +2,11 @@ from googletrans import Translator, constants
 
 # init the Google API translator
 translator = Translator()
+def trans(text):
+    translation = translator.translate(text, dest="es", src="en")
+    return translation.text
 
-translation = translator.translate("Acacia log", dest="es", src="en")
-print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
+#print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
 # print(translation.extra_data)
 # le puedes meter una lista de frases al translator.translate
 # hay una posibilidad de que si haces demasiados requests google banee tu dirección IP
--- test.py
+++ test.py
@@ -1,72 +1,76 @@
 import os
-from googletrans import Translator
+from Translate import trans
+
 
 files = []
-bad_chars = ["&0","&1","&2","&3","&4","&5","&6","&7","&8","&9","&a","&b","&c","&l","&m","&n","&o"]
+good_chars = ["&0","&1","&2","&3","&4","&5","&6","&7","&8","&9","&a","&b","&c","&e","&l","&m","&n","&o",'\n']
+bad_chars = ["&","%","/","<",">","[","]","{","}"]
+strange_char = [')"\n', 'Â» ', "'\n", '"\n', '| ']
+trailing_chars = ["\n", " ", '"', "'", "#"]
 
-def tidy_up(value):
-    if (value != "&") or (value != "/") or (value != "{") or (value != '"') or (value != "'"):
-        return True
-
-def tidy_up_v2(value):
-    if value not in ["&","/","{","}","'",'"']:
-        return True
-
-def filter_txt(line):
-    if '"' in line:
-        line = line[line.find('"'):]
-    elif "'" in line:
-        line = line[line.find("'"):]
-    else:
-        return
-    position = [i for i,x in enumerate(line) if x == "&"] 
-    for i in position:
-        if line[i:i + 2] in bad_chars and line[i + 2].isalnum():    
-            print(line)     
-            print(extract(line))
-            print("---------------------")
-            print("---------------------")
 
 def extract(line):
-    elts = []
-    next_and = 0
-    while line != "" or line != '':
-        phrase = line[line.index("&")+2:]
-        try:
-            next_and = phrase.index("&")
-            phrase = phrase[:next_and]
-        except:
-            phrase = line[line.index("&")+2:]
-            line = "  "
-        for i in phrase:
-            if phrase[0] == " " or phrase[0].isalnum():
-                if phrase == "" or phrase == '':
-                    pass
-                else:
-                    elts.append(phrase)
-        
-        line = line[next_and + 2:]
-    return elts
+    indexes = []
+    array_to_trans = []
+    #este for loop crea una lista llamada indexes con la posicion de los chars raros (los de la lista bad_chars)
+    for i in range(len(line)):
+        if line[i] in bad_chars:
+            indexes.append(i)
+
+    #este loop coge todas las frases que hay entre los characteres raros
+    for i in range(len(indexes)):
+        #pero en vd solo importan los que estan despues de & asin que solo coje esos
+        if line[indexes[i]] == "&":
+            a = (line[indexes[i]] + line[indexes[i]+1])
+            if a in good_chars and indexes[-1] != indexes[i]:
+                if (len(line[indexes[i] + 2:indexes[i+1]])) > 1 and (line[indexes[i] + 2:indexes[i+1]] != " "):
+                    array_to_trans.append([[indexes[i]+2, indexes[i+1]],line[indexes[i] + 2:indexes[i+1]]])
+            if indexes[-1] == indexes[i]:
+                if (len(line[indexes[i] + 2:])) > 1 and (line[indexes[i] + 2:] != " "):
+                    array_to_trans.append([[indexes[i]+2, indexes[i]],line[indexes[i] + 2:]])
+    return array_to_trans
+
+def filter(extracted):
+    filtered = []
+    for word in extracted:
+        if word:
+            for elt in word:
+                try:
+                    if elt[1] in strange_char:
+                        pass
+                    elif elt[1][-1] in trailing_chars:
+                        #print(f"{elt} -> {elt[1][:-1]}")
+                        if elt[1][-2] in trailing_chars:
+                            filtered.append([[elt[0][0], elt[0][1]-1],elt[1][:-2]])
+                        else:
+                            filtered.append([[elt[0][0], elt[0][1] - 2], elt[1][:-1]])
+                    else:
+                        filtered.append(elt)
+                except Exception as e:
+                    print(elt)
+    return filtered
+
 
 def insert(original_phrase, indexes):
-    translator = Translator() #creo que lo suyo sería sacar esto
     translated_phrase = original_phrase
     total_length_diff = 0
     length_diff = 0
     for i,chunk in enumerate(indexes):
-        chunk_translated = translator.translate(original_phrase[indexes[i][0]:indexes[i][1]], dest="es", src="en").text
+        chunk_translated = trans(original_phrase[indexes[i][0]:indexes[i][1]])
         translated_phrase = translated_phrase[:(indexes[i][0] + total_length_diff)] + chunk_translated + translated_phrase[(indexes[i][1] + length_diff):]
         length_diff = len(chunk_translated) - indexes[i][1] + indexes[i][0]
         total_length_diff += length_diff
     return translated_phrase
 
 def get_string(files):
+    strings = []
     for i in files:
         text = open(i, "r")
         lines = text.readlines()
         text.close()
         for line in lines:
-            filter_txt(line)
+            strings.append(extract(line))
+    return strings
 
 def reader(dir):
     try:
@@ -80,9 +84,14 @@ def reader(dir):
             reader(f"{dir}/{i}")
 
 reader('./archivos')
-get_string(files)
+resultado = get_string(files)
+filtrado = filter(resultado)
+for elt in filtrado:
+    print(elt)
 
-
+# hay algunas palabras en las que los indices estan de al reves o algo
+# lo siguiente es implementar la traducción
+# podemos hacer en dos pasadas al archivo o en cuanto encotremos las palabras (esta es la mejor pero va a ser un lio)
 
 class phr:
     
@@ -95,6 +104,6 @@ class phr:
         self.file_path = file_path
 
 #get_string("./archivos/Quests/quests/collect66rawrabbit.yml")
-print(insert("hello world", ["hello","world"], [[0,4],[6,11]]))
+#print(insert("hello world", [[0,4],[6,11]]))
 
 
